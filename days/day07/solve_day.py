2a7538560a30d7878bb64c9593d147175f338253
Oliver Oakman <oliver.oakman@seeingmachines.com>
2023-12-07 17:51:47 +1100

    Part 1 solved - no time to finish part 2

--- days/day07/solve_day.py
+++ days/day07/solve_day.py
@@ -1,5 +1,7 @@
-from collections import Counter, Dict
-from typing import List, Tuple
+from collections import Counter
+from copy import deepcopy
+from functools import cmp_to_key
+from typing import List, Tuple, Dict
 
 from solver import Solver
 
@@ -10,7 +12,8 @@ class Day07(Solver):
         self.my_base_path = __file__
         self.day = day
 
-        self.CARD_STRENGTH_ORDER = [x for x in "AKQJT98765432"]
+        self.CARD_STRENGTH_P1: List = [x for x in "AKQJT98765432"]
+        self.CARD_STRENGTH_P2: List = [x for x in "AKQT98765432J"]
 
     def __get_class_of_hand(self, hand: str) -> int:
         """
@@ -47,32 +50,56 @@ class Day07(Solver):
 
     def __compare_hands(self, hand1: Dict, hand2: Dict) -> Tuple[str]:
         if hand1["hand_class"] == hand2["hand_class"]:
-            for h1, h2 in zip(hand1["hand"], hand2["hand"]):
-                print(h1, h2)
+            for h1, h2 in zip(hand1["original_hand"], hand2["original_hand"]):
+                rel_strength = self.CARD_STRENGTH.index(h2) - self.CARD_STRENGTH.index(h1)
+                if rel_strength != 0:
+                    return rel_strength
+                assert "Cannot break tie!"
 
         return hand1["hand_class"] - hand2["hand_class"]
 
-    def part1(self, data: List[str]) -> None:
+    def __make_hands(self, data: List[str]) -> None:
         cards_breakdown = []
-        for idx, line in enumerate(data):
+        for line in data:
             hand, bid = line.split(" ")
             cards_breakdown.append(
                 {
                     "hand": hand,
+                    "original_hand": hand,
                     "bid": int(bid),
-                    "original_index": idx,
                     "hand_class": self.__get_class_of_hand(hand),
                 }
             )
-
-        print(
-            self.__compare_hands(
-                {"hand": "T55J5", "hand_class": 3}, {"hand": "QQQJA", "hand_class": 3}
-            )
+        cards_breakdown = sorted(
+            cards_breakdown, key=cmp_to_key(lambda x, y: self.__compare_hands(x, y))
         )
+        self.cards_breakdown = cards_breakdown
+
+    def __make_best_hand(self, bkdown: Dict) -> Dict:
+        if "J" not in bkdown["hand"]:
+            return deepcopy(bkdown)
+
+        other_candidate_cards = [x for x in Counter(bkdown["hand"]).keys() if x != "J"]
+
+        print(other_candidate_cards)
+
+        updated_card = {}
+
+    def part1(self, data: List[str]) -> None:
+        self.__make_hands(data)
+
+        res_sum = 0
+
+        for idx, bkdown in enumerate(self.cards_breakdown):
+            res_sum += bkdown["bid"] * (idx + 1)
+
+        return res_sum
 
     def part2(self, data: List[str]) -> None:
-        pass
+        self.__make_best_hand({"hand": "JJTQA", "bid": 1, "hand_class": 1})
+        # updated_cards = []
+        # for bkdown in self.cards_breakdown:
+        #     updated_cards.append(self.__make_best_hand(bkdown))
 
 
 def solve_day(day: int, use_sample: bool, run_each: List[bool]):

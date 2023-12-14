9affb494df5539934dd58c3088fb36e2a4bbed53
OliverOA <OliverAOakman@gmail.com>
2023-12-14 19:58:27 +1100

    Solve part 1

--- days/day14/solve_day.py
+++ days/day14/solve_day.py
@@ -1,7 +1,8 @@
 from collections import Counter
-from typing import List
+from typing import List, Dict
 
 import numpy as np
+from tqdm import tqdm
 
 from solver import Solver
 from utils.parsers import NumpyArrayParser
@@ -13,29 +14,53 @@ class Day14(Solver):
         self.my_base_path = __file__
         self.day = day
 
-    def part1(self, data: List[str]) -> None:
-        grid = NumpyArrayParser(data).parse()
-        print(grid)
+    def __tilt_mirror(self, grid: np.array, pre_check: Dict = {}) -> np.array:
+        grid_str = str(grid)
+        if grid_str in pre_check:
+            return pre_check[grid_str]
 
-        for col in range(grid.shape[1]):
-            # Find all cases of cube rocks
-            blockers = [0] + [x for x in np.where(grid[:, col] == "#")[0].tolist()]
+        mutate_grid = np.copy(grid)
 
-            if len(blockers) == 1:
-                # Roll all to the top
-                col_breakdown = Counter(grid[:, col])
-                new_col = np.array(["O"] * col_breakdown["O"] + ["."] * col_breakdown["."])
-                grid[:, col] = new_col
-                continue
+        for col in range(mutate_grid.shape[1]):
+            blockers = [0] + [x for x in np.where(mutate_grid[:, col] == "#")[0].tolist()]
 
             new_col = []
-            for block_idx in range(len(blockers) - 1):
-                print(col, blockers[block_idx], blockers[block_idx + 1])
+            for block_idx in range(len(blockers)):
+                if block_idx == (len(blockers) - 1):
+                    col_range = mutate_grid[blockers[block_idx] :, col]
+                else:
+                    col_range = mutate_grid[blockers[block_idx] : blockers[block_idx + 1], col]
 
-        print(grid)
+                col_breakdown = Counter(col_range)
+                new_col.extend(["O"] * col_breakdown["O"] + ["."] * col_breakdown["."])
+                if block_idx < (len(blockers) - 1):
+                    new_col.append("#")
+            mutate_grid[:, col] = np.array(new_col)
+
+        pre_check[grid_str] = mutate_grid
+
+        return mutate_grid
+
+    def part1(self, data: List[str]) -> None:
+        grid = NumpyArrayParser(data).parse()
+
+        tilted_grid = self.__tilt_mirror(grid)
+        total_load = sum([tilted_grid.shape[1] - x for x in np.where(tilted_grid == "O")[0]])
+
+        return total_load
 
     def part2(self, data: List[str]) -> None:
-        pass
+        grid = NumpyArrayParser(data).parse()
+
+        cache = {}
+        for _idx in tqdm(range(1000000000)):
+            new_grid = self.__tilt_mirror(grid, cache)
+            grid = np.rot90(new_grid)
+            print(f"CACHE_SIZE {len(cache)}")
+
+        total_load = sum([new_grid.shape[1] - x for x in np.where(new_grid == "O")[0]])
+
+        return total_load
 
 
 def solve_day(day: int, use_sample: bool, run_each: List[bool]):

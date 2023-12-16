08b9190ef5b00a44db8b5fc83a031456cc404616
OliverOA <OliverAOakman@gmail.com>
2023-12-16 16:28:29 +1100

    Day 15 solved

--- days/day15/solve_day.py
+++ days/day15/solve_day.py
@@ -9,22 +9,46 @@ class Day15(Solver):
         self.my_base_path = __file__
         self.day = day
 
+    def __get_hash(self, case: str) -> int:
+        curr_num = 0
+        for c in case:
+            curr_num += ord(c)
+            curr_num *= 17
+            curr_num %= 256
+
+        return curr_num
+
     def part1(self, data: List[str]) -> None:
         cases = data[0].split(",")
-
-        case_history = []
-        for case in cases:
-            curr_num = 0
-            for c in case:
-                curr_num += ord(c)
-                curr_num *= 17
-                curr_num %= 256
-            case_history.append(curr_num)
+        case_history = [self.__get_hash(case) for case in cases]
 
         return sum(case_history)
 
     def part2(self, data: List[str]) -> None:
-        pass
+        cases = data[0].split(",")
+
+        boxes = {}
+
+        for case in cases:
+            case_breakdown = case.split("=")
+            label = case_breakdown[0]
+
+            target_box = self.__get_hash(label.replace("-", ""))
+            if target_box not in boxes:
+                boxes[target_box] = {}
+
+            if label.endswith("-"):
+                if label[:-1] in boxes[target_box]:
+                    del boxes[target_box][label[:-1]]
+            else:
+                boxes[target_box][label] = int(case_breakdown[-1])
+
+        focus_power = 0
+        for box, box_breakdown in boxes.items():
+            for slot, focal in enumerate(box_breakdown.values()):
+                focus_power += (box + 1) * (slot + 1) * focal
+
+        return focus_power
 
 
 def solve_day(day: int, use_sample: bool, run_each: List[bool]):
